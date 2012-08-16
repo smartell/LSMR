23f3a025b5410254d49495fb8249444dc67f663d
Steve Martell <martell.steve@gmail.com>
2012-08-15 16:27:47 -1000

    YEP

--- ADMB/srcGROWTH/GI.R
+++ ADMB/srcGROWTH/GI.R
@@ -1,4 +1,5 @@
 require(PBSmodelling)
+require(ggplot2)
 source("../../R/Read.admb.R")
 A <- read.admb("GI")
 class(A) <- c("list", "GI")
@@ -72,7 +73,7 @@ plot.TransitionMatrix <- function(A, ... )
 		colnames(zz) = xx
 		rownames(zz) = yy
 		plotBubbles(zz,hide0=TRUE,cpro=FALSE, rpro=FALSE,prettyaxis=FALSE,size=0.05,frange=0.01,dnam=TRUE,
-			 clrs=colr(4, 0.5), xlab="", ylab="", yaxt="n", xaxt="n", powr=0.5)
+			 clrs=colr(4, 0.5), xlab="", ylab="", yaxt="n", xaxt="n", powr=1.0)
 		title(main=iyr[jj], line=-1)
 		abline(a=0, b=1, col=colr(1, 0.5), lty=1)
 		points(linf[jj], linf[jj], pch=19, col=colr(2, 0.5))
@@ -87,7 +88,8 @@ plot.TransitionMatrix <- function(A, ... )
 	}
 	nc<-4
 	nr<-6
-	par(mfcol=c(nr,nc),mar=c(0,0,0,0),oma=c(4,4,4,4), cex.lab=0.5, mfg=c(1, 1, 6, 4), las=1, bg="white")
+	par(bg="white")
+	par(mfcol=c(nr,nc),mar=c(0,0,0,0),oma=c(4,4,4,4), cex.lab=0.5, mfg=c(1, 1, 6, 4), las=1)
 	lapply(L[-1], fn)
 	mtext("Total length (mm) in year t", 1, outer=TRUE, line=2.5)
 	mtext("Total length (mm) in year t+1", 2, outer=TRUE, line=2.5, las=0)
